d288d8620607755736b760bfa3a861b0b628f93a
Karsten Bruckmann <karsten-bruckmann@users.noreply.github.com>
2024-09-04 21:19:20 +0200

    [actions-setup] setup nx workspace

--- projects/demo/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ShipmentComponent } from './presentation/shipment/shipment.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: ShipmentComponent,
-    },
-];
--- projects/form-effects/src/lib/form-effect.type.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from 'rxjs';
-import { UntypedFormGroup } from '@angular/forms';
-
-export type FormEffect<T extends UntypedFormGroup> = (
-    formGroup: T
-) => Observable<void>;
