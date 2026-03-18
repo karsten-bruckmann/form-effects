e42d3baf9c926b29588e1bf80f4ced603897746b
Karsten Bruckmann <4608350+karsten-bruckmann@users.noreply.github.com>
2026-03-18 16:41:57 +0100

    [main] update demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { ShipmentComponent } from './presentation/shipment/shipment.component';
 
@@ -8,7 +8,7 @@ import { ShipmentComponent } from './presentation/shipment/shipment.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ShipmentComponent],
+  imports: [ReactiveFormsModule, ShipmentComponent],
 })
 export class AppComponent {
   title = 'showcases-form';
